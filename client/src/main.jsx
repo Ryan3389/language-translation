c7a07117367954459e2fc2008f3d073ead456339
Ryan3389 <Ryan.Cuthbert12@gmail.com>
2024-10-02 11:30:31 -0400

    installed tailwind and added hero page

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,40 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import App from './App'
+
+import HeroPage from "./pages/HeroPage";
+import DashboardPage from './pages/DashboardPage';
+import LoginPage from './pages/LoginPage';
+import SignupPage from './pages/SignupPage';
+// import LazyResult from 'postcss/lib/lazy-result';
 import './index.css'
 
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <App />,
+        errorElement: <h1>Error</h1>,
+        children: [
+            {
+                index: true,
+                element: <HeroPage />
+            },
+            {
+                path: '/dashboard',
+                element: <DashboardPage />
+            },
+            {
+                path: '/login',
+                element: <LoginPage />
+            },
+            {
+                path: '/signup',
+                element: <SignupPage />
+            }
+        ]
+    }
+])
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <RouterProvider router={router} />
 )
